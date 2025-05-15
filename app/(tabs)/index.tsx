8765f025c1bc01630ea97441426962eda78c0ec0
tod2a <t.copin@yahoo.fr>
2025-05-15 17:07:15 +0200

    have to set the login logic but its working with a testing token

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,21 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchGames } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: games,
+    loading: gamesLoading,
+    error: gamesError
+  } = useFetch(() => fetchGames({
+    query: ''
+  }))
 
   return (
     <View className="flex-1 bg-primary">
@@ -14,12 +24,41 @@ export default function Index() {
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
         <Image source={icons.logo} resizeMode='contain' className="w-12 h-16 p-3 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => router.push("/search")}
-            placeholder="Search for a game"
+        {gamesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-        </View>
+        ) : gamesError ? (
+          <Text className="text-lg text-white font-bold mt-5 mb-3">Error: {gamesError?.message}</Text>
+        ) :
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a game"
+            />
+
+            <>
+              <FlatList
+                data={games?.data || []}
+                renderItem={({ item }) => (
+                  <Text className="text-white text-sm">{item.name}</Text>
+                )}
+                keyExtractor={(item) => item.id}
+                numColumns={2}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap: 20,
+                  paddingRight: 5,
+                  marginBottom: 10
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
+          </View>
+        }
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -3,9 +3,10 @@ import Constants from 'expo-constants';
 const apiUrl = Constants.expoConfig?.extra?.EXPO_PUBLIC_API_URL ?? 'https://ludice.app/api/';
 
 export const API_CONFIG = {
-    BASE_URL: apiUrl,
+    BASE_URL: 'https://tst.ludice.app/api/',
     headers: {
         accept: 'application/json',
+        Authorization: 'Bearer 1|eFEereP8EmTHevaCmQdb5T1UqxnkTcOcKkg6sqUraa83b4ba',
     }
 }
 
@@ -18,7 +19,7 @@ export const fetchGames = async ({ query }: { query: string }) => {
     });
 
     if (!response.ok) {
-        throw new Error(`Failed to fetch games: ${response.statusText}`);
+        throw new Error(`Failed to fetch games ${endpoint}: ${response.statusText}`);
     }
 
     const data = await response.json();
