fa811cedf65e946c82656a3702f88c96f1e1bd81
tod2a <t.copin@yahoo.fr>
2025-05-19 17:54:06 +0200

    adding title

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -6,7 +6,7 @@ const search = () => {
   return (
     <View className='flex-1 bg-primary'>
       <Image source={images.bg} className="absolute w-full z-0" />
-      <Text>search</Text>
+      <Text className="text-3xl font-bold text-white text-center mt-8 mb-6">A quoi on joue ?</Text>
     </View>
   )
 }
