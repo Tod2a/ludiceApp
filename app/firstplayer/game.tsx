78a19ce2ec344a4b3407ea48706ab32a897e3436
tod2a <t.copin@yahoo.fr>
2025-05-20 16:46:15 +0200

    adding the page to go directly on the select first player

--- app/firstplayer/game.tsx
+++ app/firstplayer/game.tsx
@@ -1,17 +1,39 @@
+import GamesAutoComplete from '@/components/autocompletes/LibraryGamesAutocomplete'
 import { icons } from '@/constants/icons'
 import { images } from '@/constants/images'
+import { Game } from '@/interfaces'
 import { useRouter } from 'expo-router'
 import React from 'react'
 import { Image, ScrollView, Text, TouchableOpacity, View } from 'react-native'
 
 const index = () => {
     const router = useRouter();
+
+    const onAdd = (g: Game) => {
+        router.push({
+            pathname: '/firstplayer/select/[id]',
+            params: { id: g.id },
+        })
+    };
+
     return (
         <View className="flex-1 bg-primary">
             <Image source={images.bg} className="absolute w-full z-0" />
-            <ScrollView contentContainerStyle={{ paddingBottom: 60 }}>
-                <Text className='text-white flex-1 justify-center text-center'>index</Text>
+
+            <ScrollView contentContainerStyle={{
+                flexGrow: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                paddingBottom: 60,
+            }}>
+                <View className='p-20'>
+                    <Text className='text-yellow-200'>Pour quel jeu voulez-vous sélectionner le premier joueur?</Text>
+                    <View className='pt-2'>
+                        <GamesAutoComplete onAdd={onAdd} />
+                    </View>
+                </View>
             </ScrollView>
+
             <TouchableOpacity
                 className="absolute bottom-5 left-0 right-0 mx-5 bg-green-400 rounded-lg py-3.5 flex flex-row items-center justify-center z-50"
                 onPress={router.back}
@@ -23,6 +45,7 @@ const index = () => {
                 />
                 <Text className="text-white font-semibold text-base">Retour</Text>
             </TouchableOpacity>
+
         </View>
     )
 }
---
+++ components/autocompletes/LibraryGamesAutocomplete.tsx
@@ -0,0 +1,65 @@
+import { icons } from "@/constants/icons";
+import { Game } from "@/interfaces";
+import { fetchLibraryGames } from "@/services/api/library";
+import React, { useEffect, useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+interface Props {
+    onAdd: (game: Game) => void;
+}
+
+const GamesAutoComplete: React.FC<Props> = ({ onAdd }) => {
+    const [input, setInput] = useState('');
+    const [options, setOptions] = useState<Game[]>([]);
+
+    useEffect(() => {
+        if (input.length >= 2) {
+            fetchLibraryGames({ query: input }).then((res) => {
+                setOptions(res.library.data);
+            });
+        } else {
+            setOptions([]);
+        }
+    }, [input]);
+
+    const handleSelect = (game: Game) => {
+        onAdd(game);
+        setInput('');
+        setOptions([]);
+    }
+
+    return (
+        <View>
+            <View className="relative">
+                <View className="relative">
+                    <TextInput
+                        placeholder="Rechercher un jeu"
+                        value={input}
+                        onChangeText={setInput}
+                        placeholderTextColor="#F9F6C0"
+                        className="bg-dark-100 text-yellow-100"
+                    />
+                    {input.length > 0 && (
+                        <TouchableOpacity
+                            onPress={() => setInput('')}
+                            className="absolute right-2 top-1/2 -translate-y-1/2"
+                        >
+                            <Image source={icons.firecross} tintColor={'#807D80'} className="w-4 h-4 text-black" />
+                        </TouchableOpacity>
+                    )}
+                </View>
+                <View className='bg-dark-100 rounded-b-xl z-50 shadow-md absolute top-full left-0 right-0 text-yellow-100'>
+                    {options.slice(0, 5).map((g) => (
+
+                        <TouchableOpacity key={g.id} onPress={() => handleSelect(g)} >
+                            <Text className="text-yellow-100 px-2 py-1">{g.name}</Text>
+                        </TouchableOpacity>
+
+                    ))}
+                </View>
+            </View>
+        </View>
+    )
+}
+
+export default GamesAutoComplete
